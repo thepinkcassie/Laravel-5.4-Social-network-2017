8f3f880916fe554f23c9d8752c771ea6c46a16a6
Casthra Demosthene <castdemos@gmail.com>
2017-08-12 22:29:52 -0400

    Add files via upload

---
+++ NewFriendRequest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Messages\BroadcastMessage;
+
+class NewFriendRequest extends Notification
+{
+    use Queueable;
+
+    public $user;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct($user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail', 'broadcast', 'database'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+                    ->line('Hey, you received a new friend request from' . $this->user->username )
+                    ->action('Check out their profile', route('profile',['slug'=>$this->user->username]))
+                    ->line('Thank you for using Scriblyz!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            'username' => $this->user->username,
+            'message' => $this->user->username. ' sent you a friend request.'
+        ];
+    }
+}
